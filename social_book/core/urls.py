be9a2a7191d3e53f2783a67061c8749172731cea
yushi <a1125189291@gmail.com>
2024-10-19 17:15:22 -0400

    create upload function

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns =[
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name = 'signin'),
     path('logout', views.logout,name="logouts"),
-    path('settings',views.settings, name='settings')
+    path('settings',views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
     
 ]
