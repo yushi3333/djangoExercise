abaceb8c4deb3a95b9f270c9279450aecad9cdef
yushi <a1125189291@gmail.com>
2024-10-19 23:47:10 -0400

    create profile function

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -8,5 +8,7 @@ urlpatterns =[
     path('logout', views.logout,name="logouts"),
     path('settings',views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('like_post', views.like_post, name="like_post"),
+    path('profile/<str:pk>', views.profile, name = "profile"),
     
 ]
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 # send the messages
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -13,7 +13,8 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     user_object =User.objects.get(username = request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'index.html', {'user_profile': user_profile})
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
 def signup(request):
     if request.method == 'POST':
@@ -110,5 +111,39 @@ def upload(request):
         return redirect('/')
     else:
         return redirect("/")
+    
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+    post = Post.objects.get(id=post_id)
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+    if (like_filter == None):
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes + 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes - 1
+        post.save()
+        return redirect('/')
+
+def profile(request, pk):
+    user_object = User.objects.get(username = pk)
+    user_profile = Profile.objects.get(user=user_object)
+    #number of posts that belong to the user
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+    
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length
+    }
+    
+    return render(request, "profile.html", context)
+    
     
     
