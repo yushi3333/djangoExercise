145b0363aeb1d9d088707abba01e63892fd8d9d6
yushi <a1125189291@gmail.com>
2024-10-20 16:08:11 -0400

    update

--- social_book/core/admin.py
+++ social_book/core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 #import database
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(LikePost)
+admin.site.register(FollowersCount)
 
 
