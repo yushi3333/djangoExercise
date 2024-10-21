8acf23dbe80b91e15fd1113332cb9173478af9f2
yushi <a1125189291@gmail.com>
2024-10-20 23:26:28 -0400

    update the search function

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -10,5 +10,6 @@ urlpatterns =[
     path('upload', views.upload, name='upload'),
     path('like_post', views.like_post, name="like_post"),
     path('profile/<str:pk>', views.profile, name = "profile"),
-    path('follow', views.follow, name='follow')
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
 ]
